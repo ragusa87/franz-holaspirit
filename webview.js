5d08a929078d8d07982ca12310ff53cfd7a31a44
Laurent Constantin <laurent.constantin@liip.ch>
2023-06-07 13:35:52 +0200

    Fix notification badge

--- webview.js
+++ webview.js
@@ -1,7 +1,9 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
+    // <span data-testid="notification_badge_test_id" class="sc-bcXHqe bCfwRb"><span class="sc-iAEawV cwOUQV">2</span></span>
+    // div.sc-fekXMp:nth-child(2) > div:nth-child(1) > span:nth-child(2)
     const inboxMessages = document.querySelectorAll(
-      "#east a.inbox > span.count"
+      '[data-testid="notification_badge_test_id"] span'
     );
 
     if (inboxMessages) {
